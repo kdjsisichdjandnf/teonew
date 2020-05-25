5432d486a5c82ffa06fb7b9e30fa9cf8a9f64a8a
afdulfauzan <afdulfauzan.id@gmail.com>
2020-05-25 23:10:07 +0700

    modules: improvements spotifynow

--- userbot/modules/spotifynow.py
+++ userbot/modules/spotifynow.py
@@ -1,39 +1,49 @@
-import datetime
 import os
-from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot import bot, CMD_HELP
-from userbot.events import register
 
-@register(outgoing=True, pattern="^.snow(?: |$)(.*)")
+from userbot.events import register
+from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
+
+
+@register(outgoing=True, pattern=r'^.snow(:? |$)(.*)?')
 async def _(event):
     if event.fwd_from:
         return
     chat = "@SpotifyNowBot"
     now = f"/now"
-    await event.edit("```Processing...```")
-    async with bot.conversation(chat) as conv:
-          try:
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=901580071))
-              msg = await bot.send_message(chat, now)
-              respond = await response
-              """ - don't spam notif - """
-              await bot.send_read_acknowledge(conv.chat_id)
-          except YouBlockedUserError:
-              await event.reply("```Please unblock @SpotifyNowBot and try again```")
-              return
-          if respond.text.startswith("`You're`"):
-              await event.edit("```You're not listening to anything on Spotify at the moment```")
-              return
-          else:
-             await bot.forward_messages(event.chat_id, respond.message)
-             await event.client.delete_messages(conv.chat_id,
-                                                [msg.id, r.id, respond.id])
+    await event.edit("`Processing...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(now)
+            response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.reply("`Please unblock` @SpotifyNowBot`...`")
+            return
+        if response.text.startswith("You're"):
+            await event.edit("`You're not listening to anything on Spotify at the moment`")
+            return
+        else:
+            downloaded_file_name = await event.client.download_media(
+                                 response.media,
+                                 TEMP_DOWNLOAD_DIRECTORY
+            )
+            await event.client.send_file(
+                event.chat_id,
+                downloaded_file_name,
+                force_document=False,
+            )
+            """ - cleanup chat after completed - """
+            await event.client.delete_messages(conv.chat_id,
+                                               [msg.id, response.id])
     await event.delete()
+    return os.remove(downloaded_file_name)
+
 
 CMD_HELP.update({
-        "spotifynow":
-        "`>.snow`"
-        "\nUsage: Show what you're listening on spotify."
+    "spotifynow":
+    ">`.snow`"
+    "\nUsage: Show what you're listening on spotify."
+    "\n@SpotifyNowBot"
 })
