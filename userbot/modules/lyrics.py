84d283e341168b809ad4266c9a6831f7b8d82123
afdulfauzan <afdulfauzan.id@gmail.com>
2020-05-30 23:47:37 +0700

    lyrics: fix derp

--- userbot/modules/lyrics.py
+++ userbot/modules/lyrics.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-
+#
 import os
 import lyricsgenius
 
@@ -19,32 +19,26 @@ if GENIUS is not None:
 async def lyrics(lyric):
     await lyric.edit("`Getting information...`")
     if GENIUS is None:
-        return await lyric.edit(
+        await lyric.edit(
             "`Provide genius access token to Heroku ConfigVars...`")
+        return False
     if lyric.pattern_match.group(1) == "now":
         playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
         if playing is None:
-            return await lyric.edit(
+            await lyric.edit(
                 "`No information current lastfm scrobbling...`"
             )
+            return False
         artist = playing.get_artist()
         song = playing.get_title()
     else:
         artist = lyric.pattern_match.group(2)
         song = lyric.pattern_match.group(3)
     await lyric.edit(f"`Searching lyrics for {artist} - {song}...`")
-    try:
-        songs = genius.search_song(song, artist)
-    except TypeError:
-        return await lyric.edit(
-            "`Error credentials for GENIUS_ACCESS_TOKEN."
-            "Use Client Access Token - click Generate Access Token "
-            "instead of Client ID or Client Secret "
-            "from`  https://genius.com/api-clients"
-        )
+    songs = genius.search_song(song, artist)
     if songs is None:
         await lyric.edit(f"`Song`  **{artist} - {song}**  `not found...`")
-        return
+        return False
     if len(songs.lyrics) > 4096:
         await lyric.edit("`Lyrics is too big, view the file to see it.`")
         with open("lyrics.txt", "w+") as f:
@@ -54,18 +48,20 @@ async def lyrics(lyric):
             "lyrics.txt",
             reply_to=lyric.id,
         )
-        return os.remove("lyrics.txt")
+        os.remove("lyrics.txt")
+        return True
     else:
-        return await lyric.edit(
+        await lyric.edit(
             f"**Search query**:\n`{artist}` - `{song}`"
             f"\n\n```{songs.lyrics}```"
         )
+        return True
 
 
 CMD_HELP.update({
     "lyrics":
-    ".lyrics **<artist name> - <song name>**"
+    ">`.lyrics` **<artist name> - <song name>**"
     "\nUsage: Get lyrics matched artist and song."
-    "\n\n.lyrics now"
+    "\n\n>`.lyrics now`"
     "\nUsage: Get lyrics artist and song from current lastfm scrobbling."
 })
